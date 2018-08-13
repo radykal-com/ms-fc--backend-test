1a8d783e2bb33ddf45770d93bbbec007022683e0
Marc Ruiz <marc.ruiz@scmspain.com>
2018-08-13 12:02:49 +0200

    Clean Query DTO

--- src/main/java/com/scmspain/tweet/infrastructure/controller/query/TweetQueryDto.java
+++ src/main/java/com/scmspain/tweet/infrastructure/controller/query/TweetQueryDto.java
@@ -1,33 +1,21 @@
 package com.scmspain.tweet.infrastructure.controller.query;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude
 public class TweetQueryDto {
 
-    @JsonIgnore
     private Long id;
 
-    @JsonProperty
     private String publisher;
 
-    @JsonProperty(value = "tweet")
-    private String content;
+    private String tweet;
 
-    @JsonIgnore
     private Long pre2015MigrationStatus;
-
-    @JsonIgnore
-    private Boolean discarded;
-
-    @JsonIgnore
-    private Date discardedDate;
 
     public TweetQueryDto() {
     }
 
-    @JsonProperty
     public Long getId() {
         return id;
     }
@@ -44,37 +32,20 @@ public class TweetQueryDto {
         this.publisher = publisher;
     }
 
-    public String getContent() {
-        return content;
+    public String getTweet() {
+        return tweet;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setTweet(String tweet) {
+        this.tweet = tweet;
     }
 
-    @JsonProperty
     public Long getPre2015MigrationStatus() {
         return pre2015MigrationStatus;
     }
 
     public void setPre2015MigrationStatus(Long pre2015MigrationStatus) {
         this.pre2015MigrationStatus = pre2015MigrationStatus;
-    }
-
-    public Boolean getDiscarded() {
-        return this.discarded;
-    }
-
-    public void setDiscarded(Boolean discarded) {
-        this.discarded = discarded;
-    }
-
-    public Date getDiscardedDate() {
-        return this.discardedDate;
-    }
-
-    public void setDiscardedDate(Date discardedDate) {
-        this.discardedDate = discardedDate;
     }
 }
 
