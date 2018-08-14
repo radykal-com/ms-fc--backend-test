0d4c013df8704ed4df874b11b87f90f9f5edbc05
Marc Ruiz <marc.ruiz@scmspain.com>
2018-08-14 11:51:40 +0200

    Add more repository tests

--- src/test/java/com/scmspain/tweet/infrastructure/repository/InMemoryTweetRepositoryImplTest.java
+++ src/test/java/com/scmspain/tweet/infrastructure/repository/InMemoryTweetRepositoryImplTest.java
@@ -1,7 +1,9 @@
 package com.scmspain.tweet.infrastructure.repository;
 
 import com.scmspain.tweet.domain.model.Tweet;
+import java.util.Arrays;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,12 +11,16 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InMemoryTweetRepositoryImplTest {
+
+    private static String FIND_ALL_PUBLISHED = "SELECT t FROM Tweet t WHERE t.pre2015MigrationStatus<>99 AND t.discarded=false ORDER BY t.id DESC";
+    private static String FIND_ALL_DISCARDED = "SELECT t FROM Tweet t WHERE t.pre2015MigrationStatus<>99 AND t.discarded=true ORDER BY t.discardedDate DESC";
 
     private InMemoryTweetRepositoryImpl repository;
 
@@ -42,5 +48,29 @@ public class InMemoryTweetRepositoryImplTest {
         Tweet result = repository.findById(id);
         verify(entityManager, times(1)).find(Tweet.class, id);
         assertThat(result).isEqualTo(tweet);
+    }
+
+    @Test
+    public void shouldFindAllPublished() {
+        TypedQuery query = mock(TypedQuery.class);
+        when(query.getResultList()).thenReturn(Arrays.asList());
+        when(entityManager.createQuery(FIND_ALL_PUBLISHED, Tweet.class)).thenReturn(query);
+
+        repository.findAllPublished();
+
+        verify(entityManager, times(1)).createQuery(FIND_ALL_PUBLISHED, Tweet.class);
+        verify(query, times(1)).getResultList();
+    }
+
+    @Test
+    public void shouldFindAllDiscarded() {
+        TypedQuery query = mock(TypedQuery.class);
+        when(query.getResultList()).thenReturn(Arrays.asList());
+        when(entityManager.createQuery(FIND_ALL_DISCARDED, Tweet.class)).thenReturn(query);
+
+        repository.findAllDiscarded();
+
+        verify(entityManager, times(1)).createQuery(FIND_ALL_DISCARDED, Tweet.class);
+        verify(query, times(1)).getResultList();
     }
 }
